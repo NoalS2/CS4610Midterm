9fd7378066b84059c612b50c9ab15c669d7c098c
Noal Smith <noalssmith2@live.com>
2022-02-27 17:55:21 -0700

    question 1

---
+++ client/components/question1/counter.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+
+export const Counter = ({ min, max }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (min > 0) {
+      setCount(min);
+    }
+    if (max < 0) {
+      setCount(max);
+    }
+  }, []);
+
+  const countUp = () => {
+    if (count === max) {
+      return;
+    }
+    setCount(count + 1);
+  };
+
+  const countDown = () => {
+    if (count === min) {
+      return;
+    }
+    setCount(count - 1);
+  };
+
+  return (
+    <>
+      <div className="flex-col border-2 w-40">
+        <h1 className="m-1 text-6xl text-center">{count}</h1>
+        <div className="flex justify-around">
+          <button className="bg-blue-600 p-1 text-3xl w-9 mb-3" onClick={countUp}>
+            +
+          </button>
+          <button className="bg-blue-600 p-1 text-3xl w-9 mb-3" onClick={countDown}>
+            -
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
